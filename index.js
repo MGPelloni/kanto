02e54b6044b8f6b807a4c09aef071075a026a759
Marco Pelloni <marco@raindropmarketing.com>
2021-08-22 00:30:27 -0700

    Table SQL installation functions for new databases

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 const server = require('http').Server(app);
 const path = require("path");
 const bodyParser = require('body-parser');
+const fs = require('fs');
+
 app.use(bodyParser.json());
 
 // Postgres
@@ -19,8 +21,7 @@ app.use('/dist', express.static(path.join(__dirname, 'dist')))
 
 // Server
 server.listen(process.env.PORT || 8000);
-
-console.log('Server has been succesfully initialized, listening on port:', process.env.PORT);
+kanto_server_initialize();
 
 // Endpoints
 app.get('/', (req, res) => { // Gallery View
@@ -53,19 +54,17 @@ app.get('/templates', (req, res) => { // Create View
     });
 });
 
-app.get('/game', (req, res) => { // Create View
-    db.query('SELECT * FROM templates;', function(err, result){
+app.post('/game', (req, res) => { // Create View
+    db.query('SELECT * FROM templates WHERE (template_name)=($1);', [req.body.game], function(err, result){
         if (err){
             console.log(err.toString());
             return;
         }
 
-        result.rows.forEach(row => {
-            if (row.template_name == 'Pallet Town') {
-                res.json(row);
-                return;
-            }
-        });        
+        if (result.rows.length > 0) {
+            res.json(result.rows[0]);
+            return;
+        }     
     });
 });
 
@@ -98,3 +97,64 @@ app.post('/upload', (req, res) => { // Upload Endpoint
 
     res.json({success: true});
 });
+
+// Functions
+
+/**
+ * Install the Kanto tables into the database.
+ */
+function kanto_server_install() {
+    console.log('Running Kanto installation..');
+
+    db.query('CREATE TABLE templates (template_name text, game_data text);', function(err, result){
+        if (err){
+            console.log(err.toString());
+            return;
+        }  
+    
+        // Add default templates
+        fs.readdir('src/js/maps/', (err, files) => {
+            files.forEach(file => {
+                let game_data = JSON.parse(fs.readFileSync('src/js/maps/' + file, 'utf8'));
+
+                db.query('INSERT INTO templates (template_name, game_data) values ($1, $2);', [game_data.meta.name, JSON.stringify(game_data)], function(err, result){
+                    if (err){
+                        console.log(err.toString());
+                    }
+                });
+
+                console.log('Inserted template:', game_data.meta.name);
+            });
+        });
+    });
+
+    console.log('Successfully installed Kanto into the database.');
+}
+
+/**
+ * Drop the Kanto tables from the database.
+ */
+function kanto_server_drop() {
+    db.query('DROP TABLE templates;', function(err, result){
+        if (err){
+            console.log(err.toString());
+        }
+
+        console.log('Successfully uninstalled Kanto.');
+    });
+}
+
+/**
+ * Init function for the Kanto server.
+ */
+function kanto_server_initialize() {
+    db.query('SELECT * FROM templates;', function(err, result){
+        if (err) { // Tables aren't installed
+            console.log(err.toString());
+            kanto_server_install();
+            return;
+        }
+        
+        console.log('Server has been succesfully initialized, listening on port:', process.env.PORT);
+    });
+}
